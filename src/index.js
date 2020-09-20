9f2af1c4e643cf7bc8e51f0c308f4a152a8dfdec
Vladislav <filinseo@yandex.ru>
2020-09-21 00:24:44 +0400

    refactors

--- src/index.js
+++ src/index.js
@@ -2,34 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MessageField from './components/MessageField.jsx';
 
-/* class Message extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            arrMessage: ['Bottom', 'Top']
-        };
-    }
-    addMessage = () => {
-        this.setState(({arrMessage}) => {
-            return {arrMessage: [...arrMessage, 'новое сообщение']}
-        });
-    }
-    render() {
-        return (
-            <React.Fragment>
-                <ul>
-                    {
-                        this.state.arrMessage.map((msg, id) => {
-                            return <li key={id}>{msg}</li>
-                        })
-                    }
-                </ul>
-                <button onClick={this.addMessage}>ADD MESSAGE</button>
-            </React.Fragment>
-        )
-    }
-} */
-
 ReactDOM.render(
     <MessageField/>
     ,
